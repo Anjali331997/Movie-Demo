fa1c4d45b8119de3410173fb626f3e860df7c2be
Anjali331997 <anjaliap3march1997@gmail.com>
2024-03-13 14:24:40 +0530

    movie cards updated

--- src/Components/MovieCards/MovieCard.jsx
+++ src/Components/MovieCards/MovieCard.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import './MovieCard.css'
 
-const MovieCard = () => {
-  return (
-    <div>MovieCard</div>
-  )
+const handleMovieClick=()=>{
+  console.log("Movie Clicked");
 }
 
+const MovieCard = ({ data }) => {
+  return (
+    <div>
+      <div key={data.imdbID} className="movie-card" onClick={() => handleMovieClick(data.imdbID)}>
+        <img src={data.Poster} alt={data.Title} />
+        <div>
+          <h3>{data.Title}</h3>
+          <p>Year: {data.Year}</p>
+          <p>IMDb Rating: {data.imdbRating || 'N/A'}</p>
+        </div>
+      </div>
+    </div>
+  )
+}
 export default MovieCard
--- src/Components/MovieSearch/MovieSearch.jsx
+++ src/Components/MovieSearch/MovieSearch.jsx
@@ -1,20 +1,53 @@
 import React, { useEffect, useState } from 'react'
+import axios from 'axios';
 import './Movie.css'
+import MovieCard from '../MovieCards/MovieCard';
 
 const MovieSearch = () => {
-  const [search, setSearch] = useState()
+  const [search, setSearch] = useState('');
+  const [result, setResult] = useState([]);
+  const [err, setError] = useState(null);
+
+  const getData = () => {
+    axios.get(`https://www.omdbapi.com/?apiKey=38519d97&s=${search}`).then((res) => {
+      const data = res.data;
+      if (data.Response === "True") {
+        setResult(data.Search)
+        setError(null)
+      }
+      else {
+        setResult([]);
+        setError(true)
+      }
+    }).catch((err) => {
+      console.log(err)
+    })
+  }
 
   useEffect(() => {
-    console.log(search)
+    getData();
+    console.log(result)
   }, [search])
-  
+
   return (
     <>
-      <div>
-        <input type="text" placeholder='Search...' value={search} onChange={(e) => {
+      <div className='searchDiv'>
+        <input className='inputComp' type="text" placeholder='Search...' value={search} onChange={(e) => {
           setSearch(e.target.value)
         }} />
       </div>
+
+      {
+        result.length > 0 ?
+          <div className='movieContainer'>
+            {
+              result.map((ele, ind) => {
+                return <MovieCard key={ind} data={ele} />
+              })
+            }
+          </div>
+          :
+          <></>}
     </>
   )
 }
